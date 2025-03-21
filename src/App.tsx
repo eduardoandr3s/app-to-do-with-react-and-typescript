5ae04de8cb8083b50ddb7043f76035ff63200d33
EduAndr3s <eduardoandres.segoviaroman@cieep.com>
2025-03-21 13:49:30 +0100

    application to do 40% process

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react"
+import { JSX } from "react/jsx-dev-runtime"
+import { Todos } from "./components/Todos"
+import { type TodoId, type Todo as TodoType } from "./types"
+const mockTodos = [
+  {
+    id: '1',
+    title: 'Estudiar TypeScript',
+    completed: true,
+  },
+  {
+    id: '2',
+    title: 'Estudiar React',
+    completed: false,
+  },
+  {
+    id: '3',
+    title: 'Estudiar MongoDB',
+    completed: false,
+  },
+  {
+    id: '4',
+    title: 'Estudiar Node.js',
+    completed: false,
+  },
+  {
+    id: '5',
+    title: 'Estudiar Next.js',
+    completed: false,
+  },
+  {
+    id: '6',
+    title: 'Estudiar React Native',
+    completed: false,
+  },
+]
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = (): JSX.Element => {
+  const [todos, setTodos] = useState(mockTodos)
+
+  const handleRemove =  ({ id }: TodoId): void => {
+
+    const newTodos = todos.filter(todo =>todo.id !== id)
+      setTodos(newTodos)
+    
+  }
+
+  const handleCompleted = (
+    {id, completed}: Pick<TodoType, 'id' | 'completed'>
+  ): void => {
+      const newTodos = todos.map(todo =>{
+        if(todo.id === id){
+    return{
+          ...todo,
+          completed
+        }
+
+        }
+        return todo
+      })
+      setTodos(newTodos)
+  }
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="todoapp">
+            <Todos 
+            onToggleCompleteTodo={handleCompleted}
+            onRemoveTodo={handleRemove}
+            todos = {todos}/>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
